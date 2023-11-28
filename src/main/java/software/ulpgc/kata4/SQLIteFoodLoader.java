bbcb5b49e4bbb7fc1a219f60bbbdc51dc1caaaa2
SantiRamoli <sanolydani@gmail.com>
2023-11-28 20:05:44 +0000

    SQLIteFoodLoader class created.

---
+++ src/main/java/software/ulpgc/kata4/SQLIteFoodLoader.java
@@ -0,0 +1,43 @@
+package software.ulpgc.kata4;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLIteFoodLoader implements FoodLoader{
+
+    private final Connection connection;
+
+    public SQLIteFoodLoader(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public List<Food> loadall() throws SQLException {
+        return load(queryAll());
+    }
+
+    private ResultSet queryAll() throws SQLException {
+        return connection.createStatement().executeQuery(queryAll);
+    }
+
+    private static final String queryAll = "SELECT * FROM dataset WHERE FOODNAME = SCIENTIFICNAME";
+    private List<Food> load(ResultSet resultSet) throws SQLException {
+        List<Food> list = new ArrayList<>();
+        while (resultSet.next()){
+            list.add(foodFrom(resultSet));
+        }return list;
+    }
+
+    private Food foodFrom(ResultSet resultSet) throws SQLException {
+        return new Food(
+                resultSet.getString("FOODNAME"),
+                resultSet.getString("SCIENTIFICNAME"),
+                resultSet.getString("GROUP"),
+                resultSet.getString("SUBGROUP")
+        );
+
+    }
+}
